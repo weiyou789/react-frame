8578f5fc98bd0d3a19bfce7c4c89d7a934256d45
shiyong <shiyong123>
2020-03-25 18:10:52 +0800

    build

---
+++ build/precache-manifest.252ec05e02d1599fc66f63e4f446c5fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f11a0012807afc274a",
+    "url": "/static/css/2.38c060e6.chunk.css"
+  },
+  {
+    "revision": "6485bbe96e3efd9280c4",
+    "url": "/static/css/main.88ff69f6.chunk.css"
+  },
+  {
+    "revision": "05f11a0012807afc274a",
+    "url": "/static/js/2.a84ee235.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.a84ee235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6485bbe96e3efd9280c4",
+    "url": "/static/js/main.10eb5df3.chunk.js"
+  },
+  {
+    "revision": "335f1d3d5a7bb63346c0",
+    "url": "/static/js/runtime-main.fdf81ec7.js"
+  }
+]);
