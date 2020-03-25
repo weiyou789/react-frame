ba8d1386d542ef0bbd77af2a591d212fffa3b541
sunjun <990375133@qq.com>
2020-03-25 17:08:08 +0800

    会员

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,40 +1,19 @@
 import { lazy } from 'react';
-import layout from '../layout/layout';
 
 const config = [
-    // {
-    //     path: '/',
-    //     component: layout, // 空白页布局
-    //     meta: {
-    //         title: '首页',
-    //         icon: 'xx'
-    //     },
-    //     children: [
-    //         // 子菜单路由
-    //         {
-    //             path: '/test', // 路由路径
-    //             isMenu: false,
-    //             meta: {
-    //                 title: 'test',
-    //                 icon: 'xxx'
-    //             },
-    //             component: lazy(() => import('../views/test')), // 懒加载 路由组件
-    //         }
-    //     ],
-    // },
     {
         path: '/',
+        exact: true,
         meta: {
-            title: '首页',
-            icon: 'xx'
+            title: '首页'
         },
         component: lazy(() => import('../views/homePage')), // 懒加载 路由组件
     },
     {
         path: '/test', // 路由路径
+        exact: true,
         meta: {
-            title: 'test',
-            icon: 'xxx'
+            title: 'test'
         },
         component: lazy(() => import('../views/test')), // 懒加载 路由组件
     }
--- src/views/homePage/index.js
+++ src/views/homePage/index.js
@@ -1,24 +1,152 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux';
+// 导入组件
+import { PullToRefresh, ListView, Toast, Tabs } from 'antd-mobile';
+// 导入样式
 
-import * as actions from '../../redux/testRedux'
+export default class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+        this.state = {
+            tabs: [
+                { title: 'First Tab' },
+                { title: 'Second Tab' },
+                { title: 'Third Tab' },
+            ],
+            List: [],
+            pageNo: 0,
+            pageSize: 10, // 分页size
+            totalPage: 0, // 总页数初始化
+            isShowContent: false, // 控制页面再数据请求后显示
+            refreshing: false, // 是否显示刷新状态
+            dataSource,
+            isLoading: false, // 是否显示加载状态
+            height: document.documentElement.clientHeight,
+        };
+    }
 
-import { Button } from 'antd-mobile';
-
-@connect(state => state.test, dispatch => bindActionCreators(actions, dispatch))
-class Test extends Component {
-
+    componentDidMount () {
+        this.requestCouponsList();
+    }
 
 
-    // 这里要对
+    // 获取优惠券数据
+    requestCouponsList () {
+        console.log(this.state);
+        // this.props.dispatch({
+        //     type: 'coupon/ListOfUser',
+        //     payload: {
+        //         page: this.state.pageNo,
+        //         size: this.state.pageSize,
+        //     },
+        //     callback: data => {
+        //         let result = data.result;
+        //         let List = [...this.state.List, ...this.setData(result.couponUserInfoVOS)];
+        //         this.setState({
+        //             isShowContent: true,
+        //             pageNo: this.state.pageNo + 1,
+        //             List: List,
+        //             dataSource: this.state.dataSource.cloneWithRows(List), // 数据源dataSource
+        //             totalPage: Math.ceil(result.usableAmount / this.state.pageSize),
+        //             refreshing: false,
+        //             isLoading: false,
+        //         }, () => {
+        //             Toast.hide();
+        //         });
+        //     },
+        // });
+        let List = [{ title: '1' }, { title: '2' }, { title: '3' }];
+        this.setState({
+            isShowContent: true,
+            pageNo: this.state.pageNo + 1,
+            List: List,
+            dataSource: this.state.dataSource.cloneWithRows(List), // 数据源dataSource
+            totalPage: 2,
+            refreshing: false,
+            isLoading: false,
+        });
+        console.log('requestCouponsList');
+    }
+
+    // 下拉刷新
+    onRefresh = () => {
+        Toast.loading();
+        this.setState({
+            pageNo: 0,
+            totalPage: 0,
+            List: [],
+        }, () => {
+            this.requestCouponsList();
+        })
+    };
+
+    // 加载更多
+    onEndReached = () => {
+        if (this.state.isLoading || (this.state.totalPage < this.state.pageNo + 1)) {
+            Toast.hide();
+            return;
+        }
+        this.setState({
+            isLoading: true,
+        }, () => {
+            this.requestCouponsList()
+        });
+    };
+
     render () {
+        // 定义Row，从数据源(dataSurce)中接受一条数据循环到ListView
+        const row = (rowData, sectionID, rowID) => {
+            console.log('rowData: ', rowData);
+            return (
+                <div key={rowID}>
+                    <div>
+                        <div>
+                            <div>{rowData.title}</div>
+                        </div>
+                    </div>
+                </div>
+            );
+        };
         return (
-            <div className='z'>
-                index
+            <div className='home-page'>
+                {
+                    <Tabs tabs={this.state.tabs}
+                        initialPage={0}
+                        onChange={(tab, index) => { console.log('onChange', index, tab); }}
+                        onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+                    >
+                        <div style={{ height: '100vh' }}>
+                            <ListView
+                                key={this.state.isActive}
+                                ref={el => this.lv = el}
+                                dataSource={this.state.dataSource}
+                                renderFooter={() => (<div>
+                                    {this.state.isLoading ? '正在加载...' : '真的已经到底了'}
+                                </div>)}
+                                renderRow={row}
+                                useBodyScroll={true}
+                                distanceToRefresh='20'
+                                pullToRefresh={<PullToRefresh
+                                    refreshing={this.state.refreshing}
+                                    onRefresh={this.onRefresh}
+                                />}
+                                onEndReached={this.onEndReached}
+                                onEndReachedThreshold={30}
+                                pageSize={this.state.pageSize}
+                            />
+                        </div>
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+                            Content of second tab
+                    </div>
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+                            Content of third tab
+                    </div>
+                    </Tabs>
+
+                }
             </div>
         );
     }
 }
-
-export default Test
