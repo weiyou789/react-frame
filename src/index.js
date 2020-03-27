478e78d43eadbcf7a53967e43ae21e02ae11cf39
weiyou789 <weiyou789567@163.com>
2020-03-27 16:07:40 +0800

    wyz

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App'
+// import App from './App'
+import Root from './Root'
 import * as serviceWorker from './serviceWorker'
 // import 'antd-mobile/dist/antd-mobile.css'
 import '@/services/axios.js'
+import store from "./store";
 
+function renderApp(app) {
+    const mountNode = document.getElementById('root')
+    ReactDOM.render(app, mountNode)
+}
 
+renderApp(<Root store={store} />)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
 import React, { Suspense } from 'react';
 //HashRouter
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect,withRouter } from 'react-router-dom';
 
 import LoadingPage from '../layout/loadPage'
 import routers from './routers';
@@ -34,10 +34,11 @@ const renderRoutes = routes => {
                         strict={route.strict}
                         render={() => {
                             const renderChildRoutes = renderRoutes(route.children);
+                            const Withrouter = withRouter(route.component)
                             if (route.component) {
                                 return (
                                     <Suspense fallback={<LoadingPage />}>
-                                        <route.component route={route}>{renderChildRoutes}</route.component>
+                                        <Withrouter route={route}>{renderChildRoutes}</Withrouter>
                                     </Suspense>
                                 );
                             }
